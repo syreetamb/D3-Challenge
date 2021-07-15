3e0fd517bb212d9f4e319597b92d5892b8c994d4
Syreeta Berry <mzberry1@hotmail.com>
2021-07-14 22:40:38 -0400

    updated

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -1,12 +1,19 @@
-d3.select('#scatter').style('border','2px solid black');
-var width = parseFloat(d3.select('#scatter').style('width'));
-var height = .66*width;
-var svg = d3.select('#scatter').append('svg').attr('width',width).attr('height',height);
+var svgWidth = 1000;
+var svgHeight = 1000;
 
+var margin ={
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+};
 
-var xText = svg
-        .append('g')
-        .attr('transform',`translate(${width/2},${.97*height})`);
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+var svg = d3.select('#scatter').append('svg').attr('width',svgWidth).attr('height',svgHeight + 50);
+
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`)
 
 var chosenXAxis = " ";
 var chosenYAxis = " ";
@@ -32,44 +39,145 @@ function yScale(data, chosenYAxis) {
         
 }
 
-    xText
+function renderXCircles(circlesGroup, newXScale, chosenXAxis) {
+
+    circlesGroup.transition()
+        .duration()
+        .attr("d", d => newXScale(d[chosenXAxis]))
+        .attr("d", d => newXScale(d[chosenXAxis]));
+
+    return circlesGroup;
+    
+}
+
+function renderYCircles(circlesGroup, newYScale, chosenYAxis) {
+
+    circlesGroup.transition()
+        .duration()
+        .attr("d", d => newYScale(d[chosenYAxis]))
+        .attr("d", d => newYScale(d[chosenYAxis]));
+
+    return circlesGroup;
+    
+}
+
+function renderXText(circledGroup, newXScale, chosenXAxis) {
+
+    circlesGroup.transition()
+        .duration()
+        .attr("d", d => newXScale(d[chosenXAxis]));
+
+    return circlesGroup;
+    
+}
+
+function renderYText(circledGroup, newYScale, chosenYAxis) {
+
+    circlesGroup.transition()
+        .duration()
+        .attr("d", d => newYScale(d[chosenYAxis]));
+
+    return circlesGroup;
+    
+}
+
+function updateToolTip(chosenXAxis, chosenYAxis, circlesGroup) {
+
+    var xlabel;
+    var ylabel;
+
+    if (chosenXAxis === "") {
+        xlabel = ":";
+    } 
+    else if (chosenXAxis === "") {
+        xlabel = ":";
+    }
+
+    if (chosenYAxis === "") {
+        ylabel = ":";
+    }
+    else if (chosenYAxis === "") {
+        ylabel = ":";
+    }
+
+    var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .style("color", "blue")
+        .style("background", "white")
+        .style("border", "black")
+        .style("border-width", )
+}
+var bottomAxis = d3.axisBottom(xLinearScale);
+
+var leftAxis = d3.axisLeft(yLinearScale);
+
+var xAxis = chartGroup.append("g")
+    .attr("tranform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+var yAxis = chartGroup.append("g")
+    .call(leftAxis);
+
+var circlesGroup = chartGroup.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("g");
+
+var circles = circlesGroup.append("circle")
+    .attr("d", d => xLinearScale(d[chosenXAxis]))
+    .attr("d", d => yLinearScale(d[chosenYAxis]))
+    .attr("r", 10)
+    .classed("stateCircle", true);
+
+var circlesText = circlesGroup.append("text")
+    .text(d => d.abbr)
+    .attr("d", d => xLinearScale(d[chosenXAxis]))
+    .attr("d", d => yLinearScale(d[chosenYAxis])+5)
+    .classed("stateText", true);
+
+var xTextGroup = chartGroup
+        .append('g')
+        .attr('transform',`translate(${width/2},${.97*height})`);
+
+    xText1 = xTextGroup
         .append('text')
         .text('Household Income (Median)')
         .attr('class','aText inactive x')
         .attr('data-value','income');
 
-    xText
+    xText2 = xTextGroup
         .append('text')
         .attr('y',-20)
         .text('Age (Median)')
         .attr('class','aText inactive x')
         .attr('data-value','age');
 
-    xText
+    xText3 = xTextGroup
         .append('text')
         .attr('y',-40)
         .text('In Poverty (%)')
         .attr('class','aText active x')
         .attr('data-value','poverty');
 
-var yText = svg
+var yTextGroup = chartGroup
         .append('g')
         .attr('transform',`translate(${.02*width},${height/2})rotate(-90)`)
 
-    yText
+    yText1 = yTextGroup
         .append('text')
         .text('Obese (%)')
         .attr('class','aText inactive y')
         .attr('data-value','obesity');
 
-    yText
+    yText2 = yTextGroup
         .append('text')
         .attr('y', 20)
         .text('Smokers (%)')
         .attr('class','aText inactive y')
         .attr('data-value','smokes');
 
-    yText
+    yText3 = yTextGroup
         .append('text')
         .attr('y', 40)
         .text('Lacks Healthcare (%)')
