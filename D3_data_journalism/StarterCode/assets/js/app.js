a100d9efd48ec2b10889d8d0faa8e6482b8eb1d7
Syreeta Berry <mzberry1@hotmail.com>
2021-08-10 00:13:04 -0400

    update

--- D3_data_journalism/StarterCode/assets/js/app.js
+++ D3_data_journalism/StarterCode/assets/js/app.js
@@ -1,4 +1,4 @@
-d3.select("#scatter").style("border", '2px solid black')
+d3.select("#scatter")
 
 var width = parseInt(d3.select("#scatter").style("width"));
 var height = width - width / 3.9;
@@ -76,145 +76,37 @@ d3.selectAll('.aText').on('click', function () {
 });
 
 function renderData() {
-    var xVal = d3.selectAll('.x').filter('.active').attr('data-value');
-    var yVal = d3.selectAll('.y').filter('.active').attr('data-value');
+    let xVal = d3.selectAll('.x').filter('.active').attr('data-value');
+    let yVal = d3.selectAll('.y').filter('.active').attr('data-value');
+    let toolTip = d3.tip().attr('class','d3-tip')
 
     d3.csv('assets/data/data.csv').then(data => {
-        var toolTip = d3.tip().attr('class','d3-tip')
-            .html(d=>`<div>${d.state}</div><div>${xVal}: ${d[xVal]}</div>`)
-        var xData = data.map(data=> +data[xVal]);
-        var yData = data.map(data=> +data[yVal]);
+        let xData = data.map(data=> +data[xVal]);
+        let yData = data.map(data=> +data[yVal]);
+            toolTip.html(d=>`<div>${d.state}</div><div>${xVal}: ${d[xVal]}</div><div>${yVal}: ${d[yVal]}</div>`)
 
-        console.log(xData);
-   
         var xScale = d3.scaleLinear().domain([d3.min(xData) * 0.9,d3.max(xData) * 1.1]).range([0, .8*width]);
         var yScale = d3.scaleLinear().domain([d3.min(yData) - 2, d3.max(yData) + 2]).range([-.8*height, 0]);
 
         xScaleLoc.call(d3.axisBottom(xScale))
         yScaleLoc.call(d3.axisLeft(yScale))
 
-        var circles = scaleLoc.selectAll('g').data(data).enter().append('g').on('mouseover', function (d) {
-            toolTip.show(d, this);
-            d3.select(this).style('stroke','#e3e3e3');
-        });
+        // var circles = scaleLoc.selectAll('g').data(data).enter().append('g');
+        var circles = scaleLoc.selectAll('g').data(data).enter().append('g')
+            .on('mouseover', function (d) {
+                toolTip.show(d, this);
+                d3.select(this).style('stroke','#e3e3e3');
+                })
+            .on('mouseout', function (d) {
+                toolTip.hide(d, this);
+                
+            })
+            circles.append('circle').attr('r', .02*width).attr('class','stateCircle');
+            circles.append('text').attr('class','stateText');    
 
-        circles.append('circle').attr('r', .02*width).attr('class','stateCircle');
-        circles.append('text').attr('class','stateText');    
-    });
+        d3.selectAll('.stateCircle').transition().duration(1000).attr('cx',d=>xScale(d[xVal])).attr('cy',d=>yScale(d[yVal]));
+        d3.selectAll('.stateText').transition().duration(1000).attr('dx',d=>xScale(d[xVal])).attr('dy',d=>yScale(d[yVal])+5).text(d=>d.abbr);
+
+        circles.call(toolTip);
+    })
 };
-        
-//     var circlesGroup = d3.selectAll("circle")
-//         .data(xVal)
-//         .enter()
-//         .append("g");
-
-//     var circles = circlesGroup.append("circle")
-//         .attr("d", d => xScale(xVal))
-//         .attr(yVal)
-//         .attr("r", 10)
-//         .classed("stateCircle", true);
-
-//     var circlesText = circlesGroup.append("text")
-//         .text(d => d.abbr)
-//         .attr(d => xLinearScale(xData))
-//         .attr(d => yLinearScale(yData) + 5)
-//         .classed("stateText", true);
-
- 
-// function renderXCircles(circles, xScale, xText) {
-
-//     circles.transition()
-//         .duration(500)
-//         .attr("d", d => xScale(d[xText]))
-//         .attr("d", d => xScale(d[xText]));
-
-//     return circles;
-
-// };
-
-// function renderYCircles(circles, yScale, yText) {
-
-//     circles.transition()
-//         .duration(500)
-//         .attr("d", d => yScale(d[yText]))
-//         .attr("d", d => yScale(d[yText]));
-
-//     return circles;
-
-// };
-
-// function renderXText(circles, xScale, xText) {
-
-//     circles.transition()
-//         .duration(500)
-//         .attr("d", d => xScale(d[xText]));
-
-//     return circles;
-
-// };
-
-// function renderYText(circles, yScale, yText) {
-
-//     circles.transition()
-//         .duration(500)
-//         .attr("d", d => yScale(d[yText]));
-
-//     return circles;
-
-// };
-
-// function updateToolTip(xText, yText, circles) {
-
-//     var xlabel;
-//     var ylabel;
-
-//     if (xText === "income") {
-//         xlabel = "Household Income (Median):", d.income;
-//     }
-//     else if (xText === "age") {
-//         xlabel = "Age (Median):", d.age;
-//     }
-//     else if (xText === "poverty") {
-//         xlabel = "In Poverty (%):", d.poverty;
-//     }
-
-//     if (yText === "obesity") {
-//         ylabel = "Obese (%):", d.obesity;
-//     }
-//     else if (yText === "smokes") {
-//         ylabel = "Smoker (%):", d.smokes;
-//     }
-//     else if (yText === "healthcare") {
-//         ylabel = "Lacks Healthcare (%):", d.healthcare;
-//     }
-
-
-//     var toolTip = d3.tip()
-//         .attr("class", "tooltip")
-//         .offset([80, -60])
-//         .style("color", "blue")
-//         .style("background", "white")
-//         .style("border", "black")
-//         .style("border-width", width)
-//         .style("border-radius", 10)
-//         .style("padding", 0.05)
-//         .html(function (d) {
-//             return (`${d.state}<br>${xlabel} ${d[xText]}%<br>${ylabel} ${d[yText]}%`);
-//         });
-
-//     circles.call(toolTip);
-
-//     circles.on("mouseover", function (data) {
-//         toolTip.show(data);
-//     });
-//     circles.on("mouseout", function (data, index) {
-//         toolTip.hide(data);
-//     });
-
-//     return circles;
-// };
-
-
-// d3.csv('assets/data/data.csv').then(data => {
-//     console.log(data[10]);
-// });
